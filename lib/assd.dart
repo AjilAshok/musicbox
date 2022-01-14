d0eea7780da7b0fbded6f3e31db28956a8b6ff19
AjilAshok <aj4ashok@gmail.com>
2022-01-14 10:09:45 +0530

    firstcommit

---
+++ lib/assd.dart
@@ -0,0 +1,49 @@
+// Container(child: pages[crctindex]),
+//       bottomNavigationBar: 
+//       BottomNavigationBar(
+//         currentIndex: crctindex,
+//         onTap: (index){
+//           setState(() {
+//             crctindex=index;
+//           });
+//         },
+//         type: BottomNavigationBarType.fixed,
+        
+//          unselectedItemColor: Colors.white,
+//         selectedItemColor: Colors.blue,
+//         backgroundColor: Colors.black54,
+//         elevation: 0,
+        
+//   showUnselectedLabels: true,
+//           // ignore: prefer_const_literals_to_create_immutables
+//           items: [
+          
+//             const BottomNavigationBarItem(
+             
+              
+            
+              
+//                 icon: Icon(Icons.home,),
+//                 // title: Text('Favorites',style: TextStyle(color:Colors.black ),),
+//               label: 'Home',
+              
+              
+//                 ),
+//             const BottomNavigationBarItem(
+          
+               
+//                 icon: Icon(Icons.playlist_add),
+//                 // title: Text('Favorites',style: TextStyle(color:Colors.black ),),
+//                 label: 'Playlist'),
+//                   const BottomNavigationBarItem(
+                
+//                 icon: Icon(FontAwesomeIcons.heart),
+//                 // title: Text('Favorites',style: TextStyle(color:Colors.black ),),
+//                 label: 'Favourites'),
+//                   const BottomNavigationBarItem(
+                
+//                 icon: Icon(FontAwesomeIcons.cog),
+//                 // title: Text('Favorites',style: TextStyle(color:Colors.black ),),
+//                 label: 'Settings'),
+//           ]),
+//     );
